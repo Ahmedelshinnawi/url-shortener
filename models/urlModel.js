f0c9798cd67d9b5f4203ee2c8ea845fec8383b63
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-12-19 00:13:15 +0200

    making the db schema

---
+++ models/urlModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const shortId = require("shortid");
+
+const urlSchema = new mongoose.Schema({
+  fullUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+});
+
+module.exports = mongoose.model("urlModel", urlSchema);
