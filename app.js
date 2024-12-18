c3be5f032d097def14e5fd72d891a0868b5cc741
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-12-19 00:14:45 +0200

    Make the DB connection

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
+const connectDB = async () => {
+  const connect = await mongoose.connect(process.env.MONGODBURI);
+  console.log(`MongoDB Connected: ${connect.connection.host}`);
+};
 app.get("/", async (req, res) => {
   res.render("index");
 });
 
 app.listen(7000, () => {
+  connectDB();
   console.log(`Server is Running on Port 7000`);
 });
